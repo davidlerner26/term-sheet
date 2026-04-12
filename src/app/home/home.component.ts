f5d9070aab60d06dd69473b3e5943a456750b95c
David Lerner <david.lerner@hotmail.com>
2026-04-12 15:03:29 -0300

    Add empty table to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
+import { CurrencyPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CurrencyPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  displayedColumns: string[] = [
+    'dealName',
+    'price',
+    'address',
+    'noi',
+    'capRate',
+  ];
+  dataSource = [];
+}
