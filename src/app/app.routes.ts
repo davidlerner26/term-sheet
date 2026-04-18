263bae78fdbfa0f5f2464ad5466d0b954c25d104
David Lerner <david.lerner@hotmail.com>
2026-04-18 15:12:59 -0300

    Migrate routes to lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,22 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
-import { AuthComponent } from './components/auth/auth.component';
 
 const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/auth');
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    loadComponent: () =>
+      import('./components/home/home.component').then((m) => m.HomeComponent),
     data: {
       authOnly: true,
       authGuardPipe: redirectUnauthorizedToHome,
     },
     canActivate: [AuthGuard],
   },
-  { path: 'auth', component: AuthComponent },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./components/auth/auth.component').then((m) => m.AuthComponent),
+  },
 ];
